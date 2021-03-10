8a0d52809b7b8f6042cd063f45a34864f9e0b08e
ryandehmoubed99 <51096576+ryandehmoubed99@users.noreply.github.com>
2021-03-09 22:48:57 -0800

    Updated_folders

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup (
         name="thonny-JuiceMind",
-        version="0.0.6",
+        version="0.0.7",
         description="Enhanced JuiceMind features for Thonny",
         long_description="""JuiceMind plug-in which adds JuiceMind theme, automatic polling for ESP32 connection, and easy switching between computer and ESP32.
 More info about JuiceMind: www.JuiceMind.com.""",
         url="https://github.com/ryandehmoubed99/thonny-JuiceMind",
-        download_url="https://github.com/ryandehmoubed99/thonny-JuiceMind/archive/v0.0.5.tar.gz",
+        download_url="https://github.com/ryandehmoubed99/thonny-JuiceMind/archive/v0.0.6.tar.gz",
         author="Ryan Dehmoubed",
         author_email="r.dehmoubed99@gmail.com",
         license="MIT",
